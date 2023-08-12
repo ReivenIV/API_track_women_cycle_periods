ebffc47d3b76bc68b5c7b33de4fd288ae4da3829
ReivenIV <runyalen88@duck.com>
2023-08-12 17:09:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Projet work in progress
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { createPool } = require('mysql2/promise');
+require('dotenv').config();
+const fileUpload = require('express-fileupload');
+
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+app.use(
+  fileUpload({
+    createParentPath: true,
+  }),
+);
+
+// Parse les Url
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env.PORT;
+console.log(PORT);
+
+// Import endpoints
+
+// const userEndpoints = require('./endpoints/user/userEndpoints');
+// const trackTimeEndpoints = require('./endpoints/track_time/trackTimeEndpoints');
+
+(async () => {
+  try {
+    const db = await createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
+
+    setInterval(async () => {
+      await db.query('SELECT 1');
+    }, 100000);
+
+// userEndpoints(app, db);
+// trackTimeEndpoints(app, db);
+
+    app.listen(PORT, () => {
+      console.log(`Listening on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error creating database pool:', error);
+  }
+})();
