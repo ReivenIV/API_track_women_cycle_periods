932c4af6f904021ef20ab7ba7aafc61f946fe4db
ReivenIV <runyalen88@duck.com>
2023-09-16 17:26:14 +0200

    cycle updated

--- endpoints/cyclesEndpoints.js
+++ endpoints/cyclesEndpoints.js
@@ -1,4 +1,5 @@
 const errorHandler = require("../middlewares/errorHandler.js");
+const authenticateToken = require("../middlewares/authenticateToken.js");
 
 // ---------------------------
 //    cycle Endpoints
@@ -7,59 +8,72 @@ const errorHandler = require("../middlewares/errorHandler.js");
 module.exports = (app, db) => {
   const CyclesModel = require("./../models/CyclesModel")(db);
 
-  app.post("/api/v1/cycle/add", errorHandler, async (req, res, next) => {
-    try {
-      let resPost = await CyclesModel.add(req.body);
+  app.post(
+    "/api/v1/cycle/add",
+    errorHandler,
+    authenticateToken,
+    async (req, res, next) => {
+      try {
+        let resPost = await CyclesModel.add(req.body, req.userId);
 
-      if (resPost.affectedRows === 0) {
-        res.status(400).json({
-          msg: "user not updated",
+        if (resPost.affectedRows === 0) {
+          res.status(400).json({
+            msg: "user not updated",
+            affected_rows: resPost.affectedRows,
+          });
+        }
+
+        return res.status(200).json({
+          id: resPost.insertId,
+          msg: "information added to DB",
           affected_rows: resPost.affectedRows,
         });
+      } catch (error) {
+        next(error);
       }
-
-      return res.status(200).json({
-        id: resPost.insertId,
-        msg: "information added to DB",
-        affected_rows: resPost.affectedRows,
-      });
-    } catch (error) {
-      next(error);
     }
-  });
+  );
 
-  app.get("/api/v1/cycle/all_data", errorHandler, async (req, res, next) => {
-    try {
-      let responseGet = await CyclesModel.getAllData();
+  app.get(
+    "/api/v1/cycle/all_data",
+    errorHandler,
+    authenticateToken,
+    async (req, res, next) => {
+      try {
+        let responseGet = await CyclesModel.getAllData(req.userId);
 
-      if (responseGet[0].length === 0) {
-        return res.status(200).json({ msg: "User doesn't have data" });
+        if (responseGet[0].length === 0) {
+          return res.status(200).json({ msg: "User doesn't have data" });
+        }
+        return res.status(200).json(responseGet[0]);
+      } catch (error) {
+        next(error);
       }
-      return res.status(200).json(responseGet[0]);
-    } catch (error) {
-      next(error);
     }
-  });
+  );
 
   app.put(
     "/api/v1/cycle/update/:cycle_id",
     errorHandler,
+    authenticateToken,
     async (req, res, next) => {
       try {
         let resOldData = await CyclesModel.getDataByCycleId(
-          req.params.cycle_id
+          parseInt(req.params.cycle_id),
+          req.userId
         );
 
         if (resOldData.length === 0) {
           return res.status(400).json({
             msg: "Data not founded in the Database check your payload and try again.",
-            received_cycle_id: req.params.cycle_id,
+            received_cycle_id: parseInt(req.params.cycle_id),
           });
         }
 
         let resPut = await CyclesModel.updateCycleById(
           req.body.created_at,
-          req.params.cycle_id
+          parseInt(req.params.cycle_id),
+          req.userId
         );
 
         if (resPut.affectedRows === 0) {
@@ -69,7 +83,7 @@ module.exports = (app, db) => {
         }
 
         return res.status(200).json({
-          id: req.params.cycle_id,
+          id: parseInt(req.params.cycle_id),
           msg: "row aupdated",
           affected_rows: resPut.affectedRows,
         });
@@ -82,32 +96,35 @@ module.exports = (app, db) => {
   app.delete(
     "/api/v1/cycle/delete/:cycle_id",
     errorHandler,
+    authenticateToken,
     async (req, res, next) => {
       try {
         let resOldData = await CyclesModel.getDataByCycleId(
-          req.params.cycle_id
+          parseInt(req.params.cycle_id),
+          req.userId
         );
 
         if (resOldData.length === 0) {
           return res.status(400).json({
             msg: "Data not founded in the Database check your payload and try again.",
-            received_cycle_id: parseInt(req.params.cycle_id),
+            received_cycle_id: parseInt(parseInt(req.params.cycle_id)),
           });
         }
 
         let resDelete = await CyclesModel.deleteCycleById(
-          parseInt(req.params.cycle_id)
+          parseInt(parseInt(req.params.cycle_id)),
+          req.userId
         );
 
         if (resDelete.affectedRows === 0) {
           return res.status(400).json({
             msg: "We had a problem please try again",
-            received_cycle_id: parseInt(req.params.cycle_id),
+            received_cycle_id: parseInt(parseInt(req.params.cycle_id)),
           });
         }
 
         return res.status(200).json({
-          id: parseInt(req.params.cycle_id),
+          id: parseInt(parseInt(req.params.cycle_id)),
           msg: "row deleted",
           affected_rows: resDelete.affectedRows,
         });
--- models/CyclesModel.js
+++ models/CyclesModel.js
@@ -8,41 +8,42 @@ module.exports = (_db) => {
 };
 
 class CyclesModel {
-  static async add(data) {
+  static async add(data, userId) {
     const query =
-      "INSERT INTO track_cycle_periods_db.cycles (created_at) VALUES (?)";
-    const response = await db.query(query, [data.created_at]);
+      "INSERT INTO track_cycle_periods_db.cycles (created_at, user_id) VALUES (?,?)";
+    const response = await db.query(query, [data.created_at, userId]);
     return response[0];
   }
 
-  static async getAllData() {
-    const query = "SELECT * FROM track_cycle_periods_db.cycles;";
+  static async getAllData(userId) {
+    const query =
+      "SELECT * FROM track_cycle_periods_db.cycles WHERE user_id=?;";
 
-    const response = await db.query(query, []);
+    const response = await db.query(query, [userId]);
     return response;
   }
 
-  static async getDataByCycleId(cycleId) {
+  static async getDataByCycleId(cycleId, userId) {
     const query =
-      "SELECT * FROM track_cycle_periods_db.cycles WHERE cycle_id=?;";
+      "SELECT * FROM track_cycle_periods_db.cycles WHERE cycle_id=? AND user_id=?;";
 
-    const resGet = await db.query(query, [cycleId]);
+    const resGet = await db.query(query, [cycleId, userId]);
     return resGet[0];
   }
 
-  static async updateCycleById(dateUtc, cycleId) {
+  static async updateCycleById(dateUtc, cycleId, userId) {
     const query =
-      "UPDATE track_cycle_periods_db.cycles SET created_at=? WHERE cycle_id=?;";
+      "UPDATE track_cycle_periods_db.cycles SET created_at=? WHERE cycle_id=? AND user_id=?;";
 
-    const resPut = await db.query(query, [dateUtc, cycleId]);
+    const resPut = await db.query(query, [dateUtc, cycleId, userId]);
     return resPut[0];
   }
 
-  static async deleteCycleById(cycleId) {
+  static async deleteCycleById(cycleId, userId) {
     const query =
-      "DELETE FROM `track_cycle_periods_db`.`cycles` WHERE `cycle_id` =?;";
+      "DELETE FROM `track_cycle_periods_db`.`cycles` WHERE `cycle_id`=? AND user_id=?;";
 
-    const resDelete = await db.query(query, [cycleId]);
+    const resDelete = await db.query(query, [cycleId, userId]);
     return resDelete[0];
   }
 }
