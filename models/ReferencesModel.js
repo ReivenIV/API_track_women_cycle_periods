c2f6f2194fb31a335f8de3a9851fe872a9d229c0
ReivenIV <runyalen88@duck.com>
2023-09-17 16:32:22 +0200

    references work in progress + updates in http files

--- models/ReferencesModel.js
+++ models/ReferencesModel.js
@@ -7,32 +7,34 @@ module.exports = (_db) => {
   return ReferencesModel;
 };
 
+  
+let queryGetActivitiesReferences = "SELECT * FROM track_cycle_periods_db.activities_reference;";
+let queryGetEmotionsReferences = "SELECT * FROM track_cycle_periods_db.emotions_reference;";
+let queryGetFoodTendencyReferences = "SELECT * FROM track_cycle_periods_db.food_tendency_reference;";
+let queryGetSymptomsReferences = "SELECT * FROM track_cycle_periods_db.symptoms_reference;";
+  
 class ReferencesModel {
-  static async getActivitiesReferences() {
-    const query = "SELECT * FROM track_cycle_periods_db.activities_reference;";
+  
 
-    const response = await db.query(query, []);
+  
+  static async getActivitiesReferences() {
+    const response = await db.query(queryGetActivitiesReferences, []);
     return response;
   }
   static async getEmotionsReferences() {
-    const query = "SELECT * FROM track_cycle_periods_db.emotions_reference;";
-
-    const response = await db.query(query, []);
+    const response = await db.query(queryGetEmotionsReferences, []);
     return response;
   }
 
   static async getFoodTendencyReferences() {
-    const query =
-      "SELECT * FROM track_cycle_periods_db.food_tendency_reference;";
-
-    const response = await db.query(query, []);
+    const response = await db.query(queryGetFoodTendencyReferences, []);
     return response;
   }
 
   static async getSymptomsReferences() {
-    const query = "SELECT * FROM track_cycle_periods_db.symptoms_reference;";
-
-    const response = await db.query(query, []);
+    const response = await db.query(queryGetSymptomsReferences, []);
     return response;
   }
+
+  
 }
